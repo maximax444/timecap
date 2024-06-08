3cd2876ac6d812ce652a26ce991b52d6de56e364
Max <max.kuzn.msx@gmail.com>
2024-06-08 23:06:18 +0300

    modal call

--- js/main.js
+++ js/main.js
@@ -13,3 +13,25 @@ window.addEventListener('scroll', function () {
     }
 }
 );
+
+let popupClose = document.querySelectorAll('.popup__close');
+let popup = document.querySelector('.overlay-call');
+let popupOpen = document.querySelectorAll('.modal');
+popupClose.forEach((el) => {
+    el.addEventListener('click', function () {
+        el.closest(".overlay").classList.remove('overlay-active');
+    });
+});
+popupOpen.forEach((el) => {
+    el.addEventListener('click', function (e) {
+        e.preventDefault();
+        popup.classList.add('overlay-active');
+    });
+});
+if (document.querySelector('.overlay-call')) {
+    document.querySelector('.overlay-call').onclick = function (e) {
+        if (!e.target.closest('.popup')) {
+            popup.classList.remove('overlay-active');
+        };
+    };
+}
