436d7fb82c54bf13f3fd7157450fda09a2318b8c
Max <max.kuzn.msx@gmail.com>
2024-06-14 03:15:13 +0300

    add video section

--- js/scroll.js
+++ js/scroll.js
@@ -2,6 +2,7 @@ var controller = new ScrollMagic.Controller();
 var tween = TweenMax.to(".home-ya__big span", 0.5, { css: 'width:100%', ease: Linear.easeNone });
 var tween2 = TweenMax.to(".home-ya1 span", 4.5, { css: 'height:100%', ease: Linear.easeNone });
 var tween3 = TweenMax.to(".home-ya2 span", 0.5, { css: 'height:100%', ease: Linear.easeNone });
+var tween4 = TweenMax.to(".home-video__mask", 0.5, { css: 'opacity:1;transform:scale(100%);', ease: Linear.easeNone });
 
 
 new ScrollMagic.Scene({ triggerElement: ".home-ya", triggerHook: 'onLeave', duration: 800 })
@@ -16,4 +17,12 @@ new ScrollMagic.Scene({ triggerElement: ".home-ya", triggerHook: 'onLeave', dura
     .addTo(controller);
 new ScrollMagic.Scene({ triggerElement: ".home-ya", triggerHook: 'onLeave', duration: 800 })
     .setTween(tween3)
+    .addTo(controller);
+
+
+new ScrollMagic.Scene({ triggerElement: ".home-video", triggerHook: 'onLeave', duration: 800 })
+    .setPin(".home-video")
+    .addTo(controller);
+new ScrollMagic.Scene({ triggerElement: ".home-video", triggerHook: 'onLeave', duration: 800 })
+    .setTween(tween4)
     .addTo(controller);
